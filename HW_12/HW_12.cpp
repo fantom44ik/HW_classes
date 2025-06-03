06522ab52fabd9e77579b133965607764b9df637
fantom44ik <fritonfantomik@gmail.com>
2025-06-03 15:44:00 +0300

    ver. 2

--- HW_12/HW_12.cpp
+++ HW_12/HW_12.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class ActionFigure {
-public:
+private:
 	string nickname;
 	string hair_color;
 	string type; // anime, game....
@@ -11,6 +11,71 @@ public:
 	bool have_armor;
 	bool have_weapon;
 	string beautiful;
+
+public:
+	void setNickname(string new_name) {
+		nickname = new_name;
+	}
+
+	string getNickname() {
+		return nickname;
+	}
+
+	void setHairColor(string new_color) {
+		hair_color = new_color;
+	}
+
+	string getHairColor() {
+		return hair_color;
+	}
+
+	void setType(string new_type) {
+		type = new_type;
+	}
+
+	string getType() {
+		return type;
+	}
+
+	void setHeight(int new_height) {
+		height = new_height;
+	}
+
+	int getHeight() {
+		return height;
+	}
+
+	void setCanMove(bool can_move_status) {
+		can_move = can_move_status;
+	}
+
+	bool getCanMove() {
+		return can_move;
+	}
+
+	void setHaveArmor(bool armor_status) {
+		have_armor = armor_status;
+	}
+
+	bool getHaveArmor() {
+		return have_armor;
+	}
+
+	void setHaveWeapon(bool weapon_status) {
+		have_weapon = weapon_status;
+	}
+
+	bool getHaveWeapon() {
+		return have_weapon;
+	}
+
+	void setBeautiful(string new_beautiful) {
+		beautiful = new_beautiful;
+	}
+
+	string getBeautiful() {
+		return beautiful;
+	}
 
 	void action() {
 		cout << "Action figure " << nickname << " is acting.\n";
@@ -26,7 +91,7 @@ public:
 };
 
 class Sword {
-public:
+private:
 	string name;
 	string color;
 	string type; // one-handed, two-handed....
@@ -34,6 +99,63 @@ public:
 	bool have_sheath;
 	bool sharpness;
 	string beautiful;
+
+public:
+	void setName(string new_name) {
+		name = new_name;
+	}
+
+	string getName() {
+		return name;
+	}
+
+	void setColor(string new_color) {
+		color = new_color;
+	}
+
+	string getColor() {
+		return color;
+	}
+
+	void setType(string new_type) {
+		type = new_type;
+	}
+
+	string getType() {
+		return type;
+	}
+
+	void setSize(int new_size) {
+		size = new_size;
+	}
+
+	int getSize() {
+		return size;
+	}
+
+	void setHaveSheath(bool sheath_status) {
+		have_sheath = sheath_status;
+	}
+
+	bool getHaveSheath() {
+		return have_sheath;
+	}
+
+	void setSharpness(bool sharp_status) {
+		sharpness = sharp_status;
+	}
+
+	bool getSharpness() {
+		return sharpness;
+	}
+
+	void setBeautiful(string new_beautiful) {
+		beautiful = new_beautiful;
+	}
+
+	string getBeautiful() {
+		return beautiful;
+	}
 
 	void unsheath() {
 		cout << "You unsheathe the " << name << ".\n";
@@ -57,7 +179,7 @@ public:
 };
 
 class ServantFate {
-public:
+private:
 	string nickname;
 	string hair_color;
 	string classname; // Saber, Lancer, Archer...
@@ -68,6 +190,87 @@ public:
 	string weapon_type;
 	string noble_phantasm;
 	string beautiful;
+
+public:
+	void setNickname(string new_nickname) {
+		nickname = new_nickname;
+	}
+
+	string getNickname() {
+		return nickname;
+	}
+
+	void setHairColor(string new_color) {
+		hair_color = new_color;
+	}
+
+	string getHairColor() {
+		return hair_color;
+	}
+
+	void setClassname(string new_classname) {
+		classname = new_classname;
+	}
+
+	string getClassname() {
+		return classname;
+	}
+
+	void setHeight(int new_height) {
+		height = new_height;
+	}
+
+	int getHeight() {
+		return height;
+	}
+
+	void setHaveArmor(bool armor_status) {
+		have_armor = armor_status;
+	}
+
+	bool getHaveArmor() {
+		return have_armor;
+	}
+
+	void setHaveWeapon(bool weapon_status) {
+		have_weapon = weapon_status;
+	}
+
+	bool getHaveWeapon() {
+		return have_weapon;
+	}
+
+	void setWeaponName(string new_weapon_name) {
+		weapon_name = new_weapon_name;
+	}
+
+	string getWeaponName() {
+		return weapon_name;
+	}
+
+	void setWeaponType(string new_weapon_type) {
+		weapon_type = new_weapon_type;
+	}
+
+	string getWeaponType() {
+		return weapon_type;
+	}
+
+	void setNoblePhantasm(string new_noble_phantasm) {
+		noble_phantasm = new_noble_phantasm;
+	}
+
+	string getNoblePhantasm() {
+		return noble_phantasm;
+	}
+
+	void setBeautiful(string new_beautiful) {
+		beautiful = new_beautiful;
+	}
+
+	string getBeautiful() {
+		return beautiful;
+	}
 
 	void talk() {
 		cout << "Servant " << nickname << " is talking.\n";
@@ -91,7 +294,7 @@ public:
 };
 
 class MasterFate {
-public:
+private:
 	string nickname;
 	string hair_color;
 	string type; // mage, human....
@@ -103,6 +306,103 @@ public:
 	bool have_servant;
 	string servant_name;
 	string beautiful;
+
+public:
+	void setNickname(string new_nickname) {
+		nickname = new_nickname;
+	}
+
+	string getNickname() {
+		return nickname;
+	}
+
+	void setHairColor(string new_color) {
+		hair_color = new_color;
+	}
+
+	string getHairColor() {
+		return hair_color;
+	}
+
+	void setType(string new_type) {
+		type = new_type;
+	}
+
+	string getType() {
+		return type;
+	}
+
+	void setHeight(int new_height) {
+		height = new_height;
+	}
+
+	int getHeight() {
+		return height;
+	}
+
+	void setHaveArmor(bool armor_status) {
+		have_armor = armor_status;
+	}
+
+	bool getHaveArmor() {
+		return have_armor;
+	}
+
+	void setHaveWeapon(bool weapon_status) {
+		have_weapon = weapon_status;
+	}
+
+	bool getHaveWeapon() {
+		return have_weapon;
+	}
+
+	void setComandSpells(int new_comand_spells) {
+		comand_spells = new_comand_spells;
+	}
+
+	int getComandSpells() {
+		if (comand_spells < 0) {
+			cout << "Comand spells cannot be negative. Setting to 0.\n";
+			comand_spells = 0;
+		}
+		else if (comand_spells > 3) {
+			cout << "Maximum comand spells is 3. Setting to 3.\n";
+			comand_spells = 3;
+		}
+		return comand_spells;
+	}
+
+	void setAge(int new_age) {
+		age = new_age;
+	}
+
+	int getAge() {
+		return age;
+	}
+
+	void setHaveServant(bool servant_status) {
+		have_servant = servant_status;
+	}
+
+	bool getHaveServant() {
+		return have_servant;
+	}
+
+	void setServantName(string new_servant_name) {
+		servant_name = new_servant_name;
+	}
+
+	string getServantName() {
+		return servant_name;
+	}
+
+	void setBeautiful(string new_beautiful) {
+		beautiful = new_beautiful;
+	}
+
+	string getBeautiful() {
+		return beautiful;
+	}
 
 	void summonServant() {
 		cout << "Master " << nickname << " summons their servant.\n";
@@ -126,7 +426,7 @@ public:
 };
 
 class Car{
-public:
+private:
 	string name;
 	string color;
 	string type; // sedan, sport.... 
@@ -136,6 +436,79 @@ public:
 	string motor;
 	string model;
 	string beautiful;
+
+public:
+	void setName(string new_name) {
+		name = new_name;
+	}
+
+	string getName() {
+		return name;
+	}
+
+	void setColor(string new_color) {
+		color = new_color;
+	}
+
+	string getColor() {
+		return color;
+	}
+
+	void setType(string new_type) {
+		type = new_type;
+	}
+
+	string getType() {
+		return type;
+	}
+
+	void setSize(int new_size) {
+		size = new_size;
+	}
+
+	int getSize() {
+		return size;
+	}
+
+	void setMaxSpeed(int new_max_speed) {
+		max_speed = new_max_speed;
+	}
+
+	int getMaxSpeed() {
+		return max_speed;
+	}
+
+	void setTuned(bool tuned_status) {
+		tuned = tuned_status;
+	}
+
+	bool getTuned() {
+		return tuned;
+	}
+
+	void setMotor(string new_motor) {
+		motor = new_motor;
+	}
+
+	string getMotor() {
+		return motor;
+	}
+
+	void setModel(string new_model) {
+		model = new_model;
+	}
+
+	string getModel() {
+		return model;
+	}
+
+	void setBeautiful(string new_beautiful) {
+		beautiful = new_beautiful;
+	}
+
+	string getBeautiful() {
+		return beautiful;
+	}
 
 	void start() {
 		cout << "You start the car " << name << ".\n";
@@ -174,26 +547,26 @@ public:
 
 int main() {
 	ActionFigure Jeanne;
-	Jeanne.nickname = "Jeanne d'arc (fate)";
-	Jeanne.hair_color = "blonde";
-	Jeanne.type = "anime";
-	Jeanne.height = 60;
-	Jeanne.can_move = true;
-	Jeanne.have_armor = true;
-	Jeanne.have_weapon = true;
-	Jeanne.beautiful = "very beautiful!!!!";
-	Jeanne.clean();
+	Jeanne.setNickname("Jeanne d'Arc (fate)");
+	Jeanne.setHairColor("blonde");
+	Jeanne.setType("Ruler");
+	Jeanne.setHeight(15);
+	Jeanne.setCanMove(true);
+	Jeanne.setHaveArmor(true);
+	Jeanne.setHaveWeapon(true);
+	Jeanne.setBeautiful("very beautiful!!!!");
 	Jeanne.action();
 	Jeanne.pat();
+	Jeanne.clean();
 
 	Sword Excalibur;
-	Excalibur.name = "Excalibur";
-	Excalibur.color = "golden";
-	Excalibur.type = "one-handed";
-	Excalibur.size = 30;
-	Excalibur.have_sheath = true;
-	Excalibur.sharpness = true;
-	Excalibur.beautiful = "very beautiful!!!!";
+	Excalibur.setName("Excalibur");
+	Excalibur.setColor("golden");
+	Excalibur.setType("one-handed");
+	Excalibur.setSize(100);
+	Excalibur.setHaveSheath(true);
+	Excalibur.setSharpness(true);
+	Excalibur.setBeautiful("very beautiful!!!!");
 	Excalibur.unsheath();
 	Excalibur.sheath();
 	Excalibur.train();
@@ -201,16 +574,16 @@ int main() {
 	Excalibur.sharp();
 
 	ServantFate Artoria;
-	Artoria.nickname = "Artoria Pendragon (fate)";
-	Artoria.hair_color = "blonde";
-	Artoria.classname = "Saber";
-	Artoria.height = 154;
-	Artoria.have_armor = true;
-	Artoria.have_weapon = true;
-	Artoria.weapon_name = "Excalibur";
-	Artoria.weapon_type = "sword";
-	Artoria.noble_phantasm = "Excalibur";
-	Artoria.beautiful = "very beautiful!!!!";
+	Artoria.setNickname("Artoria Pendragon");
+	Artoria.setHairColor("blonde");
+	Artoria.setClassname("Saber");
+	Artoria.setHeight(160);
+	Artoria.setHaveArmor(true);
+	Artoria.setHaveWeapon(true);
+	Artoria.setWeaponName("Excalibur");
+	Artoria.setWeaponType("one-handed");
+	Artoria.setNoblePhantasm("Excalibur");
+	Artoria.setBeautiful("very beautiful!!!!");
 	Artoria.talk();
 	Artoria.fight();
 	Artoria.eat();
@@ -218,17 +591,17 @@ int main() {
 	Artoria.noblePhantasm();
 
 	MasterFate Shirou;
-	Shirou.nickname = "Shirou Emiya (fate)";
-	Shirou.hair_color = "red";
-	Shirou.type = "mage";
-	Shirou.height = 180;
-	Shirou.have_armor = false;
-	Shirou.have_weapon = true;
-	Shirou.comand_spells = 3;
-	Shirou.age = 17;
-	Shirou.have_servant = true;
-	Shirou.servant_name = "Artoria Pendragon";
-	Shirou.beautiful = "handsome";
+	Shirou.setNickname("Shirou Emiya");
+	Shirou.setHairColor("brown");
+	Shirou.setType("mage");
+	Shirou.setHeight(175);
+	Shirou.setHaveArmor(false);
+	Shirou.setHaveWeapon(true);
+	Shirou.setComandSpells(3);
+	Shirou.setAge(17);
+	Shirou.setHaveServant(true);
+	Shirou.setServantName("Artoria Pendragon");
+	Shirou.setBeautiful("very beautiful!!!!");
 	Shirou.summonServant();
 	Shirou.callServant();
 	Shirou.command();
@@ -236,15 +609,15 @@ int main() {
 	Shirou.sleep();
 
 	Car Nissan;
-	Nissan.name = "Nissan Fairlady";
-	Nissan.color = "gray";
-	Nissan.type = "sport";
-	Nissan.size = 2;
-	Nissan.max_speed = 300;
-	Nissan.tuned = false;
-	Nissan.motor = "V6";
-	Nissan.model = "370Z";
-	Nissan.beautiful = "very beautiful!!!!";
+	Nissan.setName("Nissan 370Z");
+	Nissan.setColor("grey");
+	Nissan.setType("sport");
+	Nissan.setSize(123123);
+	Nissan.setMaxSpeed(300);
+	Nissan.setTuned(false);
+	Nissan.setMotor("V6");
+	Nissan.setModel("Nismo");
+	Nissan.setBeautiful("very beautiful!!!!");
 	Nissan.start();
 	Nissan.drive();
 	Nissan.stop();
